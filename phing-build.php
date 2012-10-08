6169da5b5507b1e1f40cc81064f412197f25ca7b
Doug Hurst <doug.hurst@iostudio.com>
2012-10-08 11:08:54 -0500

    Added web service calls

--- phing-build.php
+++ phing-build.php
@@ -29,19 +29,30 @@ while (true) {
     $walker(
         realpath(__DIR__.DIRECTORY_SEPARATOR.'..'),
         array(
-             function($filename) {
-                 return 'phing> ['.date('r').'] '
-                     .($filename[0] == '.'
-                         ? 'Refusing to build include() for '.$filename
-                         : 'Building include() files for '.$filename)
-                     .PHP_EOL;
-             },
-             function($filename) {
-                 return 'PHP ERROR: parse(): cannot parse '.$filename.PHP_EOL;
-             },
-             function($filename) {
-                 return 'WARNING: file '.$filename.' contains deprecated flags'.PHP_EOL;
-             }
+            function($filename) {
+                return 'phing> ['.date('r').'] '
+                    .($filename[0] == '.'
+                        ? 'Refusing to build include() for '.$filename
+                        : 'Building include() files for '.$filename)
+                    .PHP_EOL;
+            },
+            function($filename) {
+                return 'PHP ERROR: parse(): cannot parse '.$filename.PHP_EOL;
+            },
+            function($filename) {
+                return 'phing> ['.date('r').'] Collating '.$filename.PHP_EOL;
+            },
+            function($filename) {
+                return 'phing> ['.date('r').'] Found '.rand(1, 9).' tasks in "'.$filename.'"'.PHP_EOL;
+            },
+            function($filename) {
+                return 'WARNING: file '.$filename.' contains deprecated flags'.PHP_EOL;
+            },
+            function($filename) {
+                echo 'phing> ['.date('r').'] Waiting for response from server...'.PHP_EOL;
+                sleep(rand(1, 3));
+                return '';
+            }
         )
     );
 }
